4d3ff5ab0ab11244cfab9c8e97a50bd57118d416
JayceFR <jaycejefferson.vicious@gmail.com>
2025-07-17 11:22:29 +0100

    integrated jproto with dynamic array

--- jproto/main.c
+++ jproto/main.c
@@ -2,20 +2,38 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include "dynarray.h"
+
 struct file{
   char *name; 
-  char *lines; // should be a dynamic array
-  int size; 
+  dynarray lines; // should be a dynamic array
 };
 typedef struct file *file; 
+
+void printLine(FILE *out, DA_ELEMENT el, int n){
+  // typecase the void *el into char *
+  char *line = (char *)el;
+  fprintf(out, line);
+}
+
+void freeLine(DA_ELEMENT el){
+  char *line = (char *) el; 
+  free(line);
+}
 
 file constructFile(){
   file newFile = malloc(sizeof(struct file));
   assert(newFile != NULL);
 
-
+  newFile->lines = create_dynarray(&freeLine, &printLine);
 
   return newFile;
+}
+
+void freeFile(file f){
+  free(f->name);
+  free_dynarray(f->lines);
+  free(f);
 }
 
 // Need to open and read the file and return the lines 
